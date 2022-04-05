a598c6dff581cc1746742f9a0f98fd7c14be9448
Alberto Mastromonaco <alb.mastromonaco@gmail.com>
2022-04-05 18:54:10 +0200

    Exercise 1.9

--- part1/unicafe/src/Statistics.js
+++ part1/unicafe/src/Statistics.js
@@ -1,13 +1,19 @@
 export const Statistics = ({good, neutral, bad, total}) => {
-    return (
-        <div>
-            <h2> Statistics </h2>
-            <p> Good: {good} </p>
-            <p> Neutral: {neutral} </p>
-            <p> Bad: {bad} </p>
-            <p> Total: { total } </p>
-            <p> Average: { ((good * 1) - (bad * 1)) / total } </p>
-            <p> Positive%: { (good / total * 100) } </p>
-        </div>
-    )
+    if (total != 0) {    
+        return (
+            <div>
+                <h2> Statistics </h2>
+                <p> Good: {good} </p>
+                <p> Neutral: {neutral} </p>
+                <p> Bad: {bad} </p>
+                <p> Total: { total } </p>
+                <p> Average: { ((good * 1) - (bad * 1)) / total } </p>
+                <p> Positive%: { (good / total * 100) } </p>
+            </div>
+        )
+    } else {
+        return (
+            <h2> No Feedback given yet </h2>
+        )
+    }
 }
