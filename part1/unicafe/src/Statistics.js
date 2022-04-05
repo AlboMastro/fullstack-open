68ee8e1cbcf6d472cc0522a4ce596547b30e8211
Alberto Mastromonaco <alb.mastromonaco@gmail.com>
2022-04-05 19:07:44 +0200

    Exercise 1.10

--- part1/unicafe/src/Statistics.js
+++ part1/unicafe/src/Statistics.js
@@ -1,14 +1,16 @@
+const StatisticLine = ({text, value}) => <p> {text}: {value}</p>
+
 export const Statistics = ({good, neutral, bad, total}) => {
     if (total != 0) {    
         return (
             <div>
                 <h2> Statistics </h2>
-                <p> Good: {good} </p>
-                <p> Neutral: {neutral} </p>
-                <p> Bad: {bad} </p>
-                <p> Total: { total } </p>
-                <p> Average: { ((good * 1) - (bad * 1)) / total } </p>
-                <p> Positive%: { (good / total * 100) } </p>
+                <StatisticLine text={'Good'} value={good} />
+                <StatisticLine text={'Neutral'} value={neutral} />
+                <StatisticLine text={'Bad'} value={bad} />
+                <StatisticLine text={'Total'} value={total} />
+                <StatisticLine text={'Average'} value={(good - bad) / total} />
+                <StatisticLine text={'Positive%'} value={(good / total * 100)} />
             </div>
         )
     } else {
