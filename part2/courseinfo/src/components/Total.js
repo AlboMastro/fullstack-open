ac22afda3bfe0d5a15a8283fea3d8c6ccd75df9c
Alberto Mastromonaco <alb.mastromonaco@gmail.com>
2022-04-11 23:14:18 +0200

    Exercises 2.4 and 2.5

--- part2/courseinfo/src/components/Total.js
+++ part2/courseinfo/src/components/Total.js
@@ -1,4 +1,2 @@
 export const Total = ({ courseinfo }) => 
-<p>Number of exercises 
-     {courseinfo.map(parts => parts.exercises).reduce((x, y) => x + y)}
-</p>;
+<p>Number of exercises: { courseinfo.map(parts => parts.exercises).reduce((x, y) => x + y) } </p>;
