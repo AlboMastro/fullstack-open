a83c2f6e97a5f8bc92ae14417fd004a2ea09e2c7
Alberto Mastromonaco <alb.mastromonaco@gmail.com>
2022-04-11 22:23:31 +0200

    Also did exercise 2.3 by mistake

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -2,8 +2,6 @@ import { Content } from './Content'
 import { Header } from './Header'
 import { Total } from './Total'
 
-
-
 export const Course = ({ course }) => 
   <>
     <Header course={course.name}/>
