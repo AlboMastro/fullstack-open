b4ce7b5d16553d94e7741ef4d29b946bca8e9a0b
Alberto Mastromonaco <alb.mastromonaco@gmail.com>
2022-04-12 16:43:44 +0200

    Exercise 2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,18 +1,19 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]) 
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas', number: '040-123456' }]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addNewName = (event) => {
     event.preventDefault()
-    
+
     if (persons.map(p => p.name).includes(newName)) {
       alert(`${newName} is already registered`) 
     } else {
-    setPersons(persons.concat(nameObj))
+    setPersons(persons.concat(personObj));
     setNewName('')
-    console.log(persons);
+    setNewNumber('')
     }
     
   }
@@ -21,8 +22,13 @@ const App = () => {
     setNewName(event.target.value)
   }
 
-  const nameObj = {
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const personObj = {
     name: newName,
+    number: newNumber
   }
 
   return (
@@ -33,13 +39,17 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNumberChange}/>
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
+      
       <h2>Numbers</h2>
       <ul> 
         { persons.map(person => 
-          <li key={person.name}> {person.name} </li>  
+          <li key={person.name}> {person.name} {person.number} </li>  
         )}
       </ul>
     </div>
